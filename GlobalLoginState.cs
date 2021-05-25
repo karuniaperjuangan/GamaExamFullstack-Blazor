138321744734dafc6a88ffbb069a394f94fe7107
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2021-05-25 09:29:39 +0700

    Change localhost reference in case of deployment

--- GlobalLoginState.cs
+++ GlobalLoginState.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace GamaExamFullstack
 {
+    public static class GlobalSetting
+    {
+        public static string baseAddress = "https://localhost:5001/";
+    }
     public static class GlobalLoginState
     {
         public static int LoggedCreatorID = -1;
